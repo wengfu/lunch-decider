06ecab27de124f6af672a11cc78b91f6b00feba0
weng.fu <weng.george.fu@gmail.com>
2017-04-30 15:46:05 -0400

    bower-concat to handle dependencies

--- gruntfile.js
+++ gruntfile.js
@@ -7,8 +7,23 @@ module.exports = function (grunt) {
         livereload: true
       },
       files: ['app/*']
+    },
+    bower_concat: {
+      dev: {
+        dest: {
+          'js': 'app/lib/lib.js',
+          'css': 'app/lib/lib.css'
+        },
+        mainFiles: {
+          'bootstrap': ['dist/css/bootstrap.css']
+        }
+      }
     }
   });
 
+  grunt.loadNpmTasks('grunt-bower-concat');
+  // grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['bower_concat:dev']);
 };
